d62db30efc711eebb9a9cd5f166d8bed01da4c08
f12borges <f12borges@gmail.com>
2021-06-16 23:45:12 -0300

    Criação do métodos excluir e editar CidadeDAO

--- src/test/java/br/com/borges/drogaria/dao/CidadeDAOTest.java
+++ src/test/java/br/com/borges/drogaria/dao/CidadeDAOTest.java
@@ -12,11 +12,13 @@ public class CidadeDAOTest {
 	@Ignore
 	public void salvar() {
 
+		Long codigoEstado = 1L;
+
 		EstadoDAO estadoDAO = new EstadoDAO();
-		Estado estado = estadoDAO.buscar(1L);
+		Estado estado = estadoDAO.buscar(codigoEstado);
 
 		Cidade cidade = new Cidade();
-		cidade.setNome("São Paulo");
+		cidade.setNome("Jaú");
 		cidade.setEstado(estado);
 
 		CidadeDAO cidadeDAO = new CidadeDAO();
@@ -27,8 +29,8 @@ public class CidadeDAOTest {
 	@Ignore
 	public void listar() {
 		CidadeDAO cidadeDAO = new CidadeDAO();
-		List <Cidade> resultado = cidadeDAO.listar();
-		
+		List<Cidade> resultado = cidadeDAO.listar();
+
 		for (Cidade cidade : resultado) {
 			System.out.println("Código da Cidade: " + cidade.getCodigo() + ".");
 			System.out.println("Nome da Cidade: " + cidade.getNome() + ".");
@@ -38,21 +40,55 @@ public class CidadeDAOTest {
 			System.out.println();
 		}
 	}
-	
+
 	@Test
+	@Ignore
 	public void buscar() {
-		
+
 		Long codigo = 1L;
-		
+
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		Cidade cidade = cidadeDAO.buscar(codigo);
-		
+
 		System.out.println("\nCódigo da Cidade: " + cidade.getCodigo() + ".");
 		System.out.println("Nome da Cidade: " + cidade.getNome() + ".");
 		System.out.println("Código do Estado: " + cidade.getEstado().getCodigo() + ".");
 		System.out.println("Sigla do Estado: " + cidade.getEstado().getSigla() + ".");
 		System.out.println("Nome do Estado: " + cidade.getNome() + ".");
 		System.out.println();
+
+	}
+
+	@Test
+	@Ignore
+	public void excluir() {
+
+		Long codigo = 5L;
+
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigo);
+
+		cidadeDAO.excluir(cidade);
+
+	}
+
+	@Test
+	@Ignore
+	public void editar() {
+
+		Long codigoCidade = 1L;
+		Long codigoEstado = 1L;
+
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigoCidade);
+
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigoEstado);
+
+		cidade.setNome("São Paulo");
+		cidade.setEstado(estado);
+		
+		cidadeDAO.editar(cidade);
 		
 	}
 
