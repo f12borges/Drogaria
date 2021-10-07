56a631e44939e1abb0d0ab56822146da1519ef09
f12borges <f12borges@gmail.com>
2021-10-06 22:43:56 -0300

    Introdução ManegedBean

---
+++ src/main/java/br/com/borges/drogaria/bean/EstadoBean.java
@@ -0,0 +1,10 @@
+package br.com.borges.drogaria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class EstadoBean {
+	public void salvar() {
+		System.out.println("Controle ManagedBean");
+	}
+}
