b2134219788eeee698dd42d4040733b53f46c3ad
f12borges <f12borges@gmail.com>
2021-10-13 22:32:24 -0300

    Introdução FacesMEssage

--- src/main/java/br/com/borges/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/borges/drogaria/bean/EstadoBean.java
@@ -1,10 +1,15 @@
 package br.com.borges.drogaria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class EstadoBean {
 	public void salvar() {
-		System.out.println("Controle ManagedBean");
+		String texto = "Exemplo SEVERITY_INFO";
+		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);		
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, mensagem);		
 	}
 }
