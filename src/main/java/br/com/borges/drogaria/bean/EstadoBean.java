c38c9f091a8064b9f7e5a9cf2c47a7204f266bad
f12borges <f12borges@gmail.com>
2021-10-20 20:26:09 -0300

    Introdução OmniFaces

--- src/main/java/br/com/borges/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/borges/drogaria/bean/EstadoBean.java
@@ -4,13 +4,20 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
+import org.omnifaces.util.Messages;
+
 @ManagedBean
 public class EstadoBean {
 	public void salvar() {
-		String texto = "Exemplo SEVERITY_INFO";
-		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);		
-		FacesContext contexto = FacesContext.getCurrentInstance();
-		contexto.addMessage(null, mensagem);	
 		
+//		String texto = "Exemplo SEVERITY_INFO";
+//		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, texto, texto);
+//
+//		FacesContext contexto = FacesContext.getCurrentInstance();
+//		contexto.addMessage(null, mensagem);
+		
+//		Messages.addGlobalInfo(" Exemplo: OmniFaces Messages.addGlobalInfo");
+		
+		Messages.addGlobalError(" Exemplo: OmniFaces Messages.addGlobalInfo");
 	}
 }
